e40eec10ab8b44782bd716bc5e3d86782d953759
Alfonso Egio <alfonso.egio@gmail.com>
2017-04-08 10:05:14 +0200

    Set up deployment environment and wait condition

--- qual-stress-catalogues/deployment.sh
+++ qual-stress-catalogues/deployment.sh
@@ -1,17 +1,42 @@
 #!/bin/bash
 ###WORK IN PROGRESS
 #DEPLOYMENT
-export DOCKER_HOST="tcp://sp.int3.sonata-nfv.eu:2375"
+# export DOCKER_HOST="tcp://sp.int3.sonata-nfv.eu:2375"
+# target="http://sp.int3.sonata-nfv.eu"
+target="localhost"
 
-# MONGODB (CATALOGUE-REPOS)
-# Clean database
-# Removing
-docker rm -fv son-mongo 
-sleep 5
-# Starting
-docker run -d -p 27017:27017 --name son-mongo mongo
-while ! nc -z sp.int3.sonata-nfv.eu 27017; do
-  sleep 1 && echo -n .; # waiting for mongo
-done;
+function wait_for_web() {
+    until [ $(curl --connect-timeout 15 --max-time 15 -k -s -o /dev/null -w "%{http_code}" $1) -eq $2 ]; do
+	sleep 2 && 	echo -n .;
+	let retries="$retries+1"
+	if [ $retries -eq 12 ]; then
+	    echo "Timeout waiting for $2 status on $1"
+	    exit 1
+	fi
+    done
+}
+
+containers=(son-catalogue-repos
+    son-catalogue-repos-doc
+    mongodata mongo
+    mongoexpress)
+
+for i in ${containers[@]}; do
+    echo "Stopping $i"
+    docker stop $i > /dev/null 2>&1
+    echo "Removing $i"
+    docker rm $i > /dev/null 2>&1
+done
+
+echo "Building"
+docker-compose -f ./qual-stress-catalogues/docker-compose.yml build
+
+echo "Starting containers"
+docker-compose -f ./qual-stress-catalogues/docker-compose.yml up -d
+
+echo "Waiting for $target:27017"
+wait_for_web $target:27017/ 200
+echo "Waiting for $target:4002"
+wait_for_web $target:4002/ 200
 
 export DOCKER_HOST="unix:///var/run/docker.sock"
--- qual-stress-catalogues/tests.sh
+++ qual-stress-catalogues/tests.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Contact Catalogues-DB
-status_code=$(curl -s -o /dev/null -w "%{http_code}" http://sp.int3.sonata-nfv.eu:27017/)
+# target="http://sp.int3.sonata-nfv.eu"
+target="localhost"
+
+status_code=$(curl -s -o /dev/null -w "%{http_code}" $target:27017/)
 
 if [[ $status_code != 20* ]] ;
   then
@@ -10,7 +13,7 @@ fi
 echo "Success: Catalogues-DB found"
 
 # Contact Catalogue and Repositories
-status_code=$(curl -s -o /dev/null -w "%{http_code}" http://sp.int3.sonata-nfv.eu:4002/)
+status_code=$(curl -s -o /dev/null -w "%{http_code}" $target:4002/)
 
 if [[ $status_code != 20* ]] ;
   then
