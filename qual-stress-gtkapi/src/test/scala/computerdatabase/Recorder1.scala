c0f99b3f8251c3953db749b95db11eab7da348da
felipevicens <felipe.vicens@atos.net>
2017-05-08 11:52:09 +0200

    Adding new qualification tests

---
+++ qual-stress-gtkapi/src/test/scala/computerdatabase/Recorder1.scala
@@ -0,0 +1,25 @@
+package computerdatabase
+
+import scala.concurrent.duration._
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.jdbc.Predef._
+
+class Recorder1 extends Simulation {
+
+	val httpProtocol = http
+		.baseURL("http://sp.int.alb.sonata-nfv.eu:32001")
+		.inferHtmlResources()
+		.acceptHeader("*/*")
+		.userAgentHeader("curl/7.35.0")
+
+    val uri1 = "http://sp.int.alb.sonata-nfv.eu:32001/api/v2/services"
+
+	val scn = scenario("RecordedSimulation1")
+		.exec(http("request_0")
+			.get("/api/v2/services"))
+
+	setUp(scn.inject(atOnceUsers(10))).protocols(httpProtocol)
+}
+
