9c8ab54aa2d0f9990b1c5cfc4acf2f15da6b30f6
Panagiotis Karkazis <pkarkazis@synelixis.com>
2017-07-25 15:27:46 +0300

    add logs

--- qual-stress-monitoring/tests/post_stress_test.sh
+++ qual-stress-monitoring/tests/post_stress_test.sh
@@ -2,13 +2,14 @@
 
 n=$1
 c=$2
-url=$3
+logf=$3.log
+url=$4
 
 #Stress MonMan API Level 
 #-n requests     Number of requests to perform
 #-c concurrency  Number of multiple requests to make at a time
 pwd
 echo "stress_metric 1.00" > data
-ab -p data -n $n -c $c -g $4.log -q $url >>results.log
+ab -p data -n $n -c $c -g $logf -q $url >>results.log
 printf "%s" "$(<results.log)"
 
--- qual-stress-monitoring/tests/stress_test.sh
+++ qual-stress-monitoring/tests/stress_test.sh
@@ -2,11 +2,12 @@
 
 n=$1
 c=$2
-url=$3
+logf=$3.log
+url=$4
 
 #Stress MonMan API Level 
 #-n requests     Number of requests to perform
 #-c concurrency  Number of multiple requests to make at a time
 
-ab -n $n -c $c -g $4.log -q $url >>results.log
+ab -n $n -c $c -g $logf -q $url >>results.log
 printf "%s" "$(<results.log)"
