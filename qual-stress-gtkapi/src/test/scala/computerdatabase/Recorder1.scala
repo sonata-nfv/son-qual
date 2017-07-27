08c392b4999619fe343711c315b0cab09999bc63
Felipe Vicens <felipe.vicens@atos.net>
2017-07-27 08:35:00 +0200

    Testing log page

--- qual-stress-gtkapi/src/test/scala/computerdatabase/Recorder1.scala
+++ qual-stress-gtkapi/src/test/scala/computerdatabase/Recorder1.scala
@@ -9,16 +9,16 @@ import io.gatling.jdbc.Predef._
 class Recorder1 extends Simulation {
 
 	val httpProtocol = http
-		.baseURL("http://sp.int.alb.sonata-nfv.eu")
+		.baseURL("http://sp.int3.sonata-nfv.eu:5600")
 		.inferHtmlResources()
 		.acceptHeader("*/*")
 		.userAgentHeader("curl/7.35.0")
 
-    val uri1 = "http://sp.int.alb.sonata-nfv.eu/api/v2/services"
+    val uri1 = "http://sp.int3.sonata-nfv.eu:5600/admin/log"
 
 	val scn = scenario("RecordedSimulation1")
 		.exec(http("request_0")
-			.get("/api/v2/services"))
+			.get("/admin/log"))
 
 	setUp(scn.inject(atOnceUsers(10))).protocols(httpProtocol)
 }
