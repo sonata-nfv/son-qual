704532affc30ae83e7c8d1dab3f45bdb9599b013
dang03 <dang03@hotmail.com>
2017-10-11 10:57:26 +0100

    Solving Gatling services login issues

--- qual-stress-gtkapi/src/test/scala/1_services_API/Recorder1.scala
+++ qual-stress-gtkapi/src/test/scala/1_services_API/Recorder1.scala
@@ -18,42 +18,42 @@ class GetServices1 extends Simulation {
 
     val uri1 = "http://sp.int3.sonata-nfv.eu:32001/api/v2/services"
 
+    val accessToken = scenario("Authentication")
+        .exec(
+            http("requesting_access_token")
+            .post("http://sp.int3.sonata-nfv.eu:32001/api/v2/sessions")
+            .headers(Map(
+                "Accept" -> "application/json, */*; q=0.01",
+                "Content-Type" -> "application/json"
+                )
+            )
+            //.body(RawFileBody("test-credentials.txt"))
+            .body(StringBody(session =>
+                                s"""
+                                   |{
+                                   |    "username":"user01",
+                                   |    "password":"1234"
+                                   |}
+                                """.stripMargin)).asJSON
+            .check(status.is(200))
+            .check(jsonPath("$.access_token").saveAs("accessToken"))
+        )
+
+    val sessionHeaders = Map("Authorization" -> "Bearer ${accessToken}",
+                               "Content-Type" -> "application/json")
+
 	val scn = scenario("GetServices1")
-        .exec(
-             http("requesting_access_token")
-                .post("http://sp.int3.sonata-nfv.eu:32001/api/v2/sessions")
-                 .headers(Map(
-                     "Accept" -> "application/json, */*; q=0.01",
-                     "Content-Type" -> "application/json"
-                     )
-                 )
-                 //.body(RawFileBody("test-credentials.txt"))
-                 .body(StringBody(session =>
-                                     s"""
-                                        |{
-                                        |    "username":"user01",
-                                        |    "password":"1234"
-                                        |}
-                                       """.stripMargin)).asJSON
-                 .check(status.is(200))
-                 .check(
-                       jsonPath("$.access_token").saveAs("access_token")
-                     )
-         )
-        .exec(
-            session => {
-            val accessToken = session.get("access_token").asOption[String]
-            println(accessToken.getOrElse("COULD NOT FIND TOKEN"))
-            session
-        })
+        //.exec(
+        //   session => {
+        //    val accessToken = session.get("access_token").asOption[String]
+        //    println(accessToken.getOrElse("COULD NOT FIND TOKEN"))
+        //    session
+        //})
 		.exec(
 		    http("services_1")
 			.get("/api/v2/services")
-			.headers(Map(
-                "Authorization" -> "bearer ${accessToken}"
-                )
+			.headers(sessionHeaders)
 		)
-        )
 
 	setUp(scn.inject(atOnceUsers(1))).protocols(httpProtocol)
 }
