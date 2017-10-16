df1fc07329f551cfe903a7b87fadd8836c6e484b
dang03 <dang03@hotmail.com>
2017-10-16 12:48:36 +0100

    Added GK API vnfs and packages tests

---
+++ qual-stress-gtkapi/src/test/scala/2_functions_API/Recorder2.scala
@@ -0,0 +1,28 @@
+package requestsAPI
+
+import scala.concurrent.duration._
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.jdbc.Predef._
+
+class GetFunctions2 extends Simulation {
+
+    val httpProtocol = http
+		.baseURL("http://sp.int3.sonata-nfv.eu:5500")
+		.inferHtmlResources()
+		.acceptHeader("*/*")
+		.userAgentHeader("curl/7.35.0")
+
+    val uri1 = "http://sp.int3.sonata-nfv.eu:5500/functions"
+
+	val scn = scenario("GetFunctions2")
+		.exec(http("functions_2")
+			.get("/functions"))
+
+    setUp(scn.inject(
+            nothingFor(5 seconds),
+            atOnceUsers(1000))
+        )
+    .protocols(httpProtocol)
+}
